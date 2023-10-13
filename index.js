2246f7fb44bb047b8c28875560f0e6417db47bfe
Steven-Kool <stevendool15@gmail.com>
2023-10-13 17:14:34 +0630

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const mainHolder = document.getElementById('mainHolder');
+
 const navbar = document.getElementById('navbar');
 
 window.addEventListener('scroll', () => {
-   if (window.scrollY > 50) {
+   if (window.scrollY > 100) {
       navbar.classList.add('navbar-after');
-      console.log('hello');
-   } if (window.scrollY < 50 || window.scrollY == 50) {
+   } if (window.scrollY < 100 || window.scrollY == 100) {
       navbar.classList.remove('navbar-after');
    }
 });
+
+const slideHolder = document.getElementById('slideHolder');
+const slider = document.getElementById('slider');
+
+slideHolder.style.marginLeft = '-100%';
+
+let slideIndex = 0;
+
+const slideShow = (index) => {
+   if (clicked) {
+      slideHolder.addEventListener('scroll', () => {
+         let finalWidth = slideHolder.offsetWidth;
+         const userScroll = slideHolder.scrollLeft;
+
+         if ()
+      })
+   }
+}
