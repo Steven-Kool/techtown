36ae88059a125171bf3e70edcc43b4868e0b8417
Steven-Kool <stevendool15@gmail.com>
2023-10-14 17:04:45 +0630

    remove the slide but change to another idea

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const mainHolder = document.getElementById('mainHolder');
+// const mainHolder = document.getElementById('mainHolder');
 
-const navbar = document.getElementById('navbar');
+// const navbar = document.getElementById('navbar');
 
-window.addEventListener('scroll', () => {
-   if (window.scrollY > 100) {
-      navbar.classList.add('navbar-after');
-   } if (window.scrollY < 100 || window.scrollY == 100) {
-      navbar.classList.remove('navbar-after');
-   }
-});
-
-const slideHolder = document.getElementById('slideHolder');
-const slider = document.getElementById('slider');
-
-slideHolder.style.marginLeft = '-100%';
-
-let slideIndex = 0;
-
-const slideShow = (index) => {
-   if (clicked) {
-      slideHolder.addEventListener('scroll', () => {
-         let finalWidth = slideHolder.offsetWidth;
-         const userScroll = slideHolder.scrollLeft;
-
-         if ()
-      })
-   }
-}
+// window.addEventListener('scroll', () => {
+//    if (window.scrollY > 100) {
+//       navbar.classList.add('navbar-after');
+//    } if (window.scrollY < 100 || window.scrollY == 100) {
+//       navbar.classList.remove('navbar-after');
+//    }
+// });
